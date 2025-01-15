58392d9367fd3ef986bb0a480ff11a7908408293
yuzinnnee <wlsdl9248@gmail.com>
2025-01-15 15:33:18 +0900

    병합

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-// const domain = 'http://127.0.0.1:5501';
-const domain = 'https://yujinnnee.github.io/saenip/';
+const domain = 'http://127.0.0.1:5501';
+//const domain = 'https://yujinnnee.github.io/saenip/';
 
 document.addEventListener("DOMContentLoaded", function () {
   const loadPageInIframe = async (url) => {
@@ -91,20 +91,43 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // 변경시점 저장
+  let smallWidth = 0;
   function checkHeaderLines() {
     const header = document.querySelector('.header');
-
+    // 가려진 부분을 포함한 전체 넓이
     const headerFullWidth = header.scrollWidth;
     const screenWidthWithoutScrollbar = document.documentElement.clientWidth;
 
     if (headerFullWidth > screenWidthWithoutScrollbar) {
-      console.log("가려졌다!!");
+      smallWidth = headerFullWidth;
       header.classList.add('small-screen');
-    } else {
-      header.classList.remove('small-screen');
+      document.getElementById('overlay').classList.toggle('small-screen'); // 오버레이 표시/숨기기
+    }
+    else {
+      if (smallWidth < headerFullWidth) {
+        header.classList.remove('small-screen');
+        document.getElementById('overlay').classList.remove('small-screen'); // 오버레이 표시/숨기기
+        header.classList.remove('open'); // 'open' 클래스 토글하여 메뉴 표시/숨기기
+        document.getElementById('overlay').classList.remove('open'); // 오버레이 표시/숨기기
+      }
     }
   }
 
+  // 페이지 로드 시 또는 윈도우 크기 변경 시 감지
   window.addEventListener('load', checkHeaderLines);
   window.addEventListener('resize', checkHeaderLines);
+
+  // 햄버거 메뉴 클릭 시 동작
+  document.getElementById('hamburgerBtn').addEventListener('click', () => {
+
+    header.classList.toggle('open'); // 'open' 클래스 토글하여 메뉴 표시/숨기기
+    document.getElementById('overlay').classList.toggle('open'); // 오버레이 표시/숨기기
+  });
+
+  // 햄버거 메뉴 클릭 시 동작
+  document.getElementById('overlay').addEventListener('click', () => {
+    header.classList.remove('open'); // 'open' 클래스 토글하여 메뉴 표시/숨기기
+    document.getElementById('overlay').classList.remove('open'); // 오버레이 표시/숨기기
+  });
 });
